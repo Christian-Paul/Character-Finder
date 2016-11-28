ba0a13c62bd4c84a2ec5d0852e89875f7355e4d3
Christian Paul <christiansamuelpaul@gmail.com>
2016-11-27 23:03:20 -0500

    Add database

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var mongoose = require('mongoose');
-var sampleData = require('./sampleData.js');
 require('express-helpers')(app);
 app.enable('trust proxy');
 
 var port = process.env.PORT || 3000;
-var isProduction = port === 3000;
+var isDev = port === 3000;
 
 // if in dev, get credentials from local config file and start react hot loader
 // else, get credentials from environment
-if(isProduction) {
+if(isDev) {
 	var config = require('./config.js');
 
 	// react hot loader
@@ -37,23 +36,56 @@ if(isProduction) {
 app.use('/bin', express.static(path.join(__dirname, 'bin')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// database initialization
+mongoose.connect('mongodb://' + config.mongooseUsername + ':' + config.mongoosePassword + '@ds111718.mlab.com:11718/character-finder');
+
+var tropeSchema = new mongoose.Schema({
+	name: String,
+	description: String
+});
+
+var Trope = mongoose.model('Trope', tropeSchema)
+
+var characterSchema = new mongoose.Schema({
+	name: String,
+	tropes: Array,
+	tropeExplanations: Array,
+	source: String,
+	image: String,
+	description: String
+});
+
+var Character = mongoose.model('Character', characterSchema)
+
 // begin app
 app.listen(port, function(req, res) {
 	console.log('listening on 3000');
 })
 
 app.get('/tropes', function(req, res) {
-	res.send(sampleData.tropes);
+	Trope.find(function(err, tropes) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.send(tropes);
+		}
+	});
 });
 
 app.get('/characters', function(req, res) {
-	res.send(sampleData.characters);
+	Character.find(function(err, characters) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.send(characters);
+		}
+	});
 });
 
 // React Router browser history requires every get route to serve the index.html file in case a user
 // refreshes on a page or starts using the app from any non-index route
 app.get('*', function(req, res) {
-	if(isProduction) {
+	if(isDev) {
 		res.sendFile(path.resolve(__dirname, 'src', 'index.html'))
 	} else {
 		res.sendFile(path.resolve(__dirname, 'bin', 'index.html'))
